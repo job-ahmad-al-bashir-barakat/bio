5368d73ee86cd2b0005f68e638047c4fba023a58
HP-Pavilion <abasheerbarakat@gmail.com>
2019-01-21 23:35:51 +0200

    add Procfile file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            // force https
+            \URL::forceScheme('https');
+        }
+
         $this->setDefaultSchemaStringLength();
         $this->morphMap();
     }
@@ -45,6 +50,25 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
+        if($this->app->environment('production')) {
+
+            // force https
+            $this->app['request']->server->set('HTTPS', true);
+
+            // set db config
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+            $host = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $database = substr($url["path"], 1);
+
+            Config::set('database.connections.mysql.host',$host);
+            Config::set('database.connections.mysql.username',$username);
+            Config::set('database.connections.mysql.password',$password);
+            Config::set('database.connections.mysql.database',$database);
         }
     }
 }
